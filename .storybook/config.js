07cf4392c3b427cd497c2a2cce1ac00dd95b4fa7
Yang <kshjam63@naver.com>
2020-05-20 23:46:58 +0900

    add style

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,16 @@
-import { configure } from '@storybook/react';
+import {configure, setAddon} from '@storybook/react';
 import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
-function loadStories() {
-    // require('../src/stories/inputStory');
-    // require('../src/stories/NewCounterStory');
+import JSXAddon from 'storybook-addon-jsx';
 
+import '../src/sass/materialize.scss'
+
+function loadStories() {
     const context = require.context('../src/stories', true, /Story\.jsx$/);
+
     context.keys().forEach((srcFile) => {
         interopRequireDefault(context(srcFile));
     })
 }
 
+setAddon(JSXAddon);
 configure(loadStories, module);
